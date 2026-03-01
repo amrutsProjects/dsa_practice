2aab3bce01141750bece2641b8caf2a6e82f49e7
amrut <noamrut@gmail.com>
2026-03-01 22:56:42 +0530

    added new patterns

---
+++ Basics/patterns2.py
@@ -0,0 +1,35 @@
+##7
+# n = int(input("enter number of rows: "))
+# for i in range (n):
+#     star = 2*i + 1
+#     col = n - i
+#     print(" " * col + "*" * star + " " * col)
+
+##8
+# n = int(input("enter number of rows: "))
+# for i in range(n-1, -1, -1):
+#     star = 2*i + 1
+#     col = n - i
+#     print(" " * col + "*" * star + " " * col)
+
+##9
+# n = int(input("enter number of rows: "))
+# for i in range(n):
+#     star = 2*i + 1
+#     space = n - i
+#     print(" " * space + "*" * star + " " * space)
+
+# for j in range(n-1, -1, -1):
+#     star = 2*j + 1
+#     space = n - j
+#     print(" " * space + "*" * star + " " * space)
+
+##10
+# n = int(input("enter number of rows: "))
+# for i in range(n):
+#     print("*" * (i + 1))
+
+# for j in range(n-1, 0, -1):
+#     print("*" * j)
+
+
